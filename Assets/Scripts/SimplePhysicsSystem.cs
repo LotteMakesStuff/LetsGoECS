5287c12edbf7b93659dd9f37d82ce9d1f4110413
LotteMakesStuff <lotte@genericevil.com>
2020-03-08 10:30:28 +0000

    - Added a new Component type, Velocity! - Added a new System called SimplePhysicsSystem   - Currently it runs two queries/jobs.     - First it finds all entities with a Translation and a Velocity, and applies the velocity to the translation for this update     - Lastly, it takes the entities translation and copies it back to its GameObject representations Transform - Added a bunch more sprites into the test scene, so we can see how the system effects every entity that matches the component queries we made

---
+++ Assets/Scripts/SimplePhysicsSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SimplePhysicsSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        
+        Entities.WithName("ApplyPhysics")
+            .ForEach((ref Translation position, in Velocity velocity) =>
+            {
+                position.Value += velocity.Value * deltaTime;
+            }).Run();
+        
+        Entities.WithName("CopyTransformToGameobject")
+            .WithoutBurst()
+            .ForEach((Transform transform, ref Translation position) =>
+            {
+                transform.position = position.Value;
+            }).Run();
+    }
+}
